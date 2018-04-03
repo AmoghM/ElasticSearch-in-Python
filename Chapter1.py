f82aaa7fd8883065f480eb824d63c6ea598bb1d5
Amogh Mishra <amoghmishra23@gmail.com>
2018-04-04 04:29:46 +0530

    Update Chapter1.py

--- Chapter1.py
+++ Chapter1.py
@@ -5,6 +5,6 @@ print es.indices.create(index="first_index",ignore=400)
 
 print es.indices.exists(index="first_index")
 
-print es.indices.delete(index="first_index")
+print es.indices.delete(index="first_index", ignore=[400,404])
 
 print es.indices.exists(index="first_index")
