42e9e8ca4b5a7b8d8068fe1a0129153ad4b53590
Amogh Mishra <amoghmishra23@gmail.com>
2018-04-04 04:29:27 +0530

    Add Chapter 1

---
+++ Chapter1.py
@@ -0,0 +1,10 @@
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+print es.indices.create(index="first_index",ignore=400)
+
+print es.indices.exists(index="first_index")
+
+print es.indices.delete(index="first_index")
+
+print es.indices.exists(index="first_index")
